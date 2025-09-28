2c16d1f43562d141c79184e02bfc050a4e3890b8
Mahavirsinh Sodha <140199309+codewithms7777@users.noreply.github.com>
2025-09-29 02:56:28 +0530

    Delete Downloads/WEB ADD directory

--- Downloads/WEB ADD/EMOTION_UI/STARTAPP.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-start "" "chrome.exe" http://127.0.0.1:5000/
-python app.py
-pause
--- Downloads/WEB ADD/WeatherAPP/pages/_app.js
+++
@@ -1,8 +0,0 @@
-// pages/_app.js
-import '../styles/globals.css';  // Ensure this line is here to import the global styles
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
